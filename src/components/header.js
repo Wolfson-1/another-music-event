b2a66bc07ef6f6c6f1a07eb540901a074cea6bfb
wolfson-1 <harrywolverson@googlemail.com>
2023-10-01 14:15:58 +0100

    cleared base react app material & interted initial header component

---
+++ src/components/header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+        <div className="header">
+
+            <div className="navbar">
+                <button>Events</button>
+                <button>About</button>
+                <button>Meet The Team</button>
+            </div>
+            <h1>
+                Not Another Music Event.ldn
+            </h1>
+        </div>
+  )
+}
